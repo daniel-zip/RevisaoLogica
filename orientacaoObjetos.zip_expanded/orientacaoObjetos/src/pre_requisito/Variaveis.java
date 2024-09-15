372c2122293b332f4b122efbc8999b8d7d4c99ca
daniel san <danieltrotre@hotmail.com>
2024-09-15 18:35:51 -0300

    Trying to make it work

---
+++ orientacaoObjetos.zip_expanded/orientacaoObjetos/src/pre_requisito/Variaveis.java
@@ -0,0 +1,16 @@
+package pre_requisito;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		int ano = 2024;
+		String disciplina = "Estrutura de Dados";
+		
+		System.out.println("Hoje é quinta-feira!. Ano: " + ano);
+		
+		System.out.println(ano);
+		
+		System.out.println(disciplina);
+		
+	}
+}
