486d4700ae66608c43f5f7d3431a2706ab5bca8e
danie laptop <danieltrotre@hotmail.com>
2024-09-16 20:24:38 -0300

    additions to print

--- orientacaoObjetos/src/array/Pratica1.java
+++ orientacaoObjetos/src/array/Pratica1.java
@@ -11,10 +11,12 @@ public class Pratica1 {
 				66, 70, 71, 73, 88, 90, 33, 76, 77, 98, 101, 123, 143, 155, 167, 190};
 		
 		// 1) Exibe na tela o tamanho do array
-		System.out.println("Array de tamanho: " + numeros.length);
+		System.out.println("1) Array de tamanho: " + numeros.length);
+		
+		System.out.println("");
 		
 		// 2) Exibe na tela todos os elementos do array numeros em ordem inversa
-		System.out.println("Elementos do array em ordem inversa: ");
+		System.out.println("2) Elementos do array em ordem inversa: ");
 		for(i = numeros.length - 1; i >= 0; i--) {
 			System.out.print(numeros[i] + "|");
 		}
@@ -23,7 +25,7 @@ public class Pratica1 {
 		System.out.println("");
 		
 		// 3) Exibe na tela somente os números pares
-		System.out.println("Elementos do array que são números pares: ");
+		System.out.println("3) Elementos do array que são números pares: ");
 		for(i = 0; i < numeros.length; i++) {
 			if((numeros[i] % 2) == 0) {
 				System.out.print(numeros[i] + "|");
@@ -34,7 +36,7 @@ public class Pratica1 {
 		System.out.println("");
 		
 		// 4) Exibe na tela somente os números ímpares
-		System.out.println("Elementos do array que são números ímpares: ");
+		System.out.println("4) Elementos do array que são números ímpares: ");
 		for(i = 0; i < numeros.length; i++) {
 			if((numeros[i] % 2) != 0) {
 				System.out.print(numeros[i] + "|");
@@ -46,7 +48,7 @@ public class Pratica1 {
 		
 		// 5) Exibe a subtração da soma dos elementos pares e ímpares
 		// 5.1) Exibe a soma dos impares
-		System.out.println("Soma de todos os números ímpares: ");
+		System.out.println("5) Soma de todos os números ímpares: ");
 		for(i = 0; i < numeros.length; i++) {
 			if((numeros[i] % 2) != 0) {
 				somaImpares += numeros[i];
@@ -57,7 +59,7 @@ public class Pratica1 {
 		System.out.println("");
 		
 		// 5.2) Exibe a soma dos pares
-		System.out.println("Soma de todos os números pares: ");
+		System.out.println("6) Soma de todos os números pares: ");
 		for(i = 0; i < numeros.length; i++) {
 			if((numeros[i] % 2) == 0) {
 				somaPares += numeros[i];
@@ -69,7 +71,7 @@ public class Pratica1 {
 		
 		// 5.3) Exibe a subtração da soma dos elementos pares e ímpares
 		int subtracao = somaPares - somaImpares;
-		System.out.println("Subtração da soma dos números pares - soma dos números ímpares: " + subtracao);
+		System.out.println("7) Subtração da soma dos números pares - soma dos números ímpares: " + subtracao);
 	}
 
 }
