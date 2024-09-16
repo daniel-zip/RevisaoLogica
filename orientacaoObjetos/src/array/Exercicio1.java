0edd11dee6c615fac64025c83d63079228adbffd
daniel san <danieltrotre@hotmail.com>
2024-09-15 22:16:47 -0300

    exercicio 1 e 2 de arrays

--- orientacaoObjetos/src/array/Exercicio1.java
+++ orientacaoObjetos/src/array/Exercicio1.java
@@ -3,8 +3,71 @@ package array;
 public class Exercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int i;
+		int somaImpares = 0;
+		int somaPares = 0;
+		
+		int numeros[] = {9, 8, 1, 45, 89, 43, 55, 10, 12, 44, 68, 
+				66, 70, 71, 73, 88, 90, 33, 76, 77, 98, 101, 123, 143, 155, 167, 190};
+		
+		// 1) Exibe na tela o tamanho do array
+		System.out.println("Array de tamanho: " + numeros.length);
+		
+		// 2) Exibe na tela todos os elementos do array numeros em ordem inversa
+		System.out.println("Elementos do array em ordem inversa: ");
+		for(i = numeros.length - 1; i >= 0; i--) {
+			System.out.print(numeros[i] + "|");
+		}
+		
+		System.out.println("");
+		System.out.println("");
+		
+		// 3) Exibe na tela somente os números pares
+		System.out.println("Elementos do array que são números pares: ");
+		for(i = 0; i < numeros.length; i++) {
+			if((numeros[i] % 2) == 0) {
+				System.out.print(numeros[i] + "|");
+			}
+		}
+		
+		System.out.println("");
+		System.out.println("");
+		
+		// 4) Exibe na tela somente os números
+		System.out.println("Elementos do array que são números ímpares: ");
+		for(i = 0; i < numeros.length; i++) {
+			if((numeros[i] % 2) != 0) {
+				System.out.print(numeros[i] + "|");
+			}
+		}
+		
+		System.out.println("");
+		System.out.println("");
+		
+		// 5) Exibe a subtração da soma dos elementos pares e ímpares
+		// 5.1) Exibe a soma dos impares
+		System.out.println("Soma de todos os números ímpares: ");
+		for(i = 0; i < numeros.length; i++) {
+			if((numeros[i] % 2) != 0) {
+				somaImpares += numeros[i];
+			}
+		}
+		System.out.println(somaImpares);
 
+		System.out.println("");
+		
+		// 5.2) Exibe a soma dos pares
+		System.out.println("Soma de todos os números pares: ");
+		for(i = 0; i < numeros.length; i++) {
+			if((numeros[i] % 2) == 0) {
+				somaPares += numeros[i];
+			}
+		}
+		System.out.println(somaPares);
+		
+		// 5.3) Exibe a subtração da soma dos elementos pares e ímpares
+		int subtracao = somaPares - somaImpares;
+		System.out.println("Subtração dos números pares - soma dos números ímpares: " + subtracao);
 	}
 
 }
--- orientacaoObjetos/src/array/Exercicio2.java
+++ orientacaoObjetos/src/array/Exercicio2.java
@@ -1,10 +1,68 @@
 package array;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Exercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		double numeros[] = {7, 6.2, 0, 7.4, 8, 10, 8.2, 9.2, 5};
+		double somaNumeros = 0;
+		double mediaNumeros;
+		int i;
+		
+		
+		// a) Exiba todos os números do vetor
+		System.out.println("Elementos do array: ");
+		for(i = 0; i < numeros.length; i++) {
+			System.out.print(numeros[i] + "|");
+		}
+		
+		System.out.println("");
+		System.out.println("");
+		
+		// b) Calcule a soma de todos os elementos do vetor
+		for(i = 0; i < numeros.length; i++) {
+			somaNumeros += numeros[i];
+		}
+		System.out.println("Soma dos números do vetor: " + somaNumeros);
+		
+		System.out.println("");
+		
+		// c) Calcule a média de todos os elementos do vetor
+		mediaNumeros = (somaNumeros / numeros.length);
+		System.out.println("Média dos números do vetor: " + mediaNumeros);
+		
+		System.out.println("");
+		
+		// d) Exiba na tela a dobro de cada nota do vetor.
+		System.out.println("Dobro de cada valor do vetor: ");
+		for(i = 0; i < numeros.length; i++) {
+			System.out.print((numeros[i] * 2) + "|");
+		}
+		
+		System.out.println("");
+		System.out.println("");
+		
+		// e) Mostre o índice e o valor do maior elemento do vetor
+		double valorMaximo = numeros[0];
+		
+		for(i = 0; i < numeros.length; i++) {
+			if(numeros[i] > valorMaximo) {
+				valorMaximo = numeros[i];
+			}
+			
+		}
+		
+		for(i = 0; i < numeros.length; i++) {
+			if(valorMaximo == numeros[i]) {
+				System.out.println("Maior valor do array: " + valorMaximo);
+				System.out.println("Localizado no índice: " + i);
+			}
+		
+		}
+		
 	}
 
 }
