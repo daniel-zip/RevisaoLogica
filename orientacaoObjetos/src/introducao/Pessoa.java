74037bc61bd0837822aeceaaa69207522cc3c939
danie laptop <danieltrotre@hotmail.com>
2024-09-19 19:52:52 -0300

    created PessoaTesteArray

--- orientacaoObjetos/src/introducao/Pessoa.java
+++ orientacaoObjetos/src/introducao/Pessoa.java
@@ -5,19 +5,24 @@ public class Pessoa {
 	private String email;
 	private int idade;
 	
-	public Pessoa( ) {
-		nome = "";
-		email = "";
-		idade = -1;
+	public Pessoa() {
+		
 	}
 	
 	public Pessoa(String nome, String email, int idade) {
 		this.nome = nome;
 		this.email = email;
 		this.idade = idade;
-		
 	}
-	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 	public String getEmail() {
 		return email;
 	}
@@ -34,15 +39,6 @@ public class Pessoa {
 		this.idade = idade;
 	}
 
-	public void saudacao(String nome) {
-		System.out.println("Bom dia " + nome);
-	}
 	
-	public void setNome (String nome) {
-		this.nome = nome;
-	}
 	
-	public String getNome() {
-		return nome;
-	}
 }
---
+++ orientacaoObjetos/src/introducao/PessoaTesteArray.java
@@ -0,0 +1,10 @@
+package introducao;
+
+public class PessoaTesteArray {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
